4f54b8220fc7956f9e4dde3783060a3b144f3911
Jo Vandeginste <jo.vandeginste@gmail.com>
2020-01-04 13:35:15 +0100

    Split off, enable service
    Signed-off-by: Jo Vandeginste <jo.vandeginste@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,5 @@
 class profile {
   include ::profile::packages
   include ::profile::systemd
-
-  file {
-    '/lib/systemd/system/puppet-apply.timer':
-      ensure  => present,
-      content => file('profile/puppet-apply.timer'),
-      notify  => Class['profile::systemd'],
-      ;
-    '/lib/systemd/system/puppet-apply.service':
-      ensure  => present,
-      content => file('profile/puppet-apply.service'),
-      notify  => Class['profile::systemd'],
-      ;
-  }
+  include ::profile::puppetapply
 }
---
+++ manifests/puppetapply.pp
@@ -0,0 +1,24 @@
+class profile::puppetapply {
+  file {
+    '/lib/systemd/system/puppet-apply.timer':
+      ensure  => present,
+      content => file('profile/puppet-apply.timer'),
+      notify  => [
+        Class['profile::systemd'],
+        Service['puppet-apply'],
+      ]
+      ;
+    '/lib/systemd/system/puppet-apply.service':
+      ensure  => present,
+      content => file('profile/puppet-apply.service'),
+      notify  => [
+        Class['profile::systemd'],
+        Service['puppet-apply'],
+      ]
+      ;
+  }
+
+  service { 'puppet-apply':
+    enable => true,
+  }
+}
