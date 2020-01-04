c62e71eb4a357faf6be0f8adbe4b13c6dbe2285e
Jo Vandeginste <jo.vandeginste@gmail.com>
2020-01-04 13:31:36 +0100

    Fix paths
    Signed-off-by: Jo Vandeginste <jo.vandeginste@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,12 @@ class profile {
   include ::profile::systemd
 
   file {
-    '/usr/lib/systemd/system/puppet-apply.timer':
+    '/lib/systemd/system/puppet-apply.timer':
       ensure  => present,
       content => file('profile/puppet-apply.timer'),
       notify  => Class['profile::systemd'],
       ;
-    '/usr/lib/systemd/system/puppet-apply.service':
+    '/lib/systemd/system/puppet-apply.service':
       ensure  => present,
       content => file('profile/puppet-apply.service'),
       notify  => Class['profile::systemd'],
