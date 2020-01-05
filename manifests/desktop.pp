b675345dad28204321d70597164ca7d2b6d6e01a
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 15:41:36 +0100

    Use map
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -1,6 +1,8 @@
 class profile::desktop {
   notify { "Desktop environments: ${facts['desktop_sessions']}": }
-  $facts['desktop_sessions'].each |String $ds| {
-    include ::profile::desktop::$ds
+  $ds_classes = $facts['desktop_sessions'].map |String $ds| {
+    "profile::desktop::${ds}"
   }
+
+  include $ds_classes
 }
