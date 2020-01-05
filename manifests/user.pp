3a626b68ff5a8d74b0327d22edfee18a15fada31
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 10:46:54 +0100

    More parameters, remove welcome splash
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,5 +1,8 @@
 class profile::user (
+  String $coderdojo_user     = 'coderdojo',
+  String $coderdojo_group    = $coderdojo_user,
   String $coderdojo_password = '$6$XTj1xyhI$Ku8xdtmejyS0h/1wBIcLt8LBPoV2n7UBzKM7cCrQyd3CEyd.NeRrldfL05SHaQVGkgSopvDVt8fRX/6mbzuPL/',
+  String $coderdojo_home     = "/home/${coderdojo_user}",
 ) {
   $groups = $facts['os']['family'] ? {
     'Debian' => ['adm', 'cdrom', 'sudo', 'dip', 'plugdev', 'lpadmin', 'sambashare'],
@@ -7,15 +10,16 @@ class profile::user (
     default  => [],
   }
 
-  group { 'coderdojo':
+  group { $coderdojo_group
     ensure => present,
     gid    => 1000,
   }
 
-  user { 'coderdojo':
+  user { $coderdojo_user:
     ensure         => present,
+    command        => 'CoderDojo',
     password       => Sensitive($coderdojo_password),
-    home           => '/home/coderdojo',
+    home           => $coderdojo_home,
     uid            => 1000,
     gid            => 1000,
     groups         => $groups,
@@ -34,5 +38,9 @@ class profile::user (
     membership     => 'inclusive',
     managehome     => true,
     purge_ssh_keys => true,
+  }
+
+  file { "${coderdojo_home}/.config/autostart/lite-welcome.desktop":
+    ensure => absent,
   }
 }
