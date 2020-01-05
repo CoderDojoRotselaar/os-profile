f44d9d892f0e0cff7fc32e253d447fd677e9f58c
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 11:39:30 +0100

    Switch to inifile module
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/lightdm.pp
+++ manifests/lightdm.pp
@@ -1,18 +1,20 @@
 class profile::lightdm {
-  include ::lightdm
-
   $coderdojo_user = $::profile::user::coderdojo_user
 
-  class { '::lightdm':
-    config => {
-      'SeatDefaults' => {
-        'user-session'           => 'xfce',
-        'allow-guest'            => 'false',
-        'autologin-user'         => $coderdojo_user,
-        'autologin-user-timeout' => '0',
-        'autologin-session'      => 'lightdm-autologin',
-        'greeter-setup-script'   => '/usr/bin/numlockx on',
-      }
+  $lightdm_config = {
+    'SeatDefaults' => {
+      'user-session'           => 'xfce',
+      'allow-guest'            => 'false',
+      'autologin-user'         => $coderdojo_user,
+      'autologin-user-timeout' => '0',
+      'autologin-session'      => 'lightdm-autologin',
+      'greeter-setup-script'   => '/usr/bin/numlockx on',
     }
   }
+
+  $lightdm_defaults = {
+    'path' => '/tmp/foo.ini'
+  }
+
+  create_ini_settings($lightdm_config, $lightdm_defaults)
 }
