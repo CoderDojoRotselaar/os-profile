ce52a72694b6c893a6b977446bf71ab1bd0e0c22
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 16:19:33 +0100

    Add first version of ripgrep deployer
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

---
+++ manifests/ripgrep.pp
@@ -0,0 +1,11 @@
+class profile::ripgrep (
+  $version = '11.0.2'
+) {
+  $deb = "https://github.com/BurntSushi/ripgrep/releases/download/${version}/ripgrep_${version}_amd64.deb"
+
+  package { 'scratux':
+    provider => 'apt',
+    ensure   => installed,
+    source   => $deb,
+  }
+}
