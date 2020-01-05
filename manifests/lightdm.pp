f5e0a29bc1dbdd0b2c59bbd1ed5a7b91cedbe817
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 12:19:21 +0100

    Use a separate file
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/lightdm.pp
+++ manifests/lightdm.pp
@@ -3,18 +3,13 @@ class profile::lightdm {
 
   $lightdm_config = {
     'SeatDefaults' => {
-      'user-session'           => 'xfce',
-      'allow-guest'            => 'false',
-      'autologin-user'         => $coderdojo_user,
-      'autologin-user-timeout' => '0',
-      'autologin-session'      => 'lightdm-autologin',
-      'greeter-setup-script'   => '/usr/bin/numlockx on',
+      'autologin-user' => $coderdojo_user,
     }
   }
 
   $lightdm_defaults = {
+    path              => '/etc/lightdm/lightdm.conf.d/autologin.conf'
     key_val_separator => '=',
-    path              => '/tmp/foo.ini'
   }
 
   create_ini_settings($lightdm_config, $lightdm_defaults)
