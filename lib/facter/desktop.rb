c12e4257c45eb3000047ccc79ff39d16622f7b82
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 15:33:51 +0100

    Fix the fact
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- lib/facter/desktop.rb
+++ lib/facter/desktop.rb
@@ -1,22 +1,23 @@
+require 'shellwords'
+
 supported_desktop_sessions = [
   'gnome-shell',
   'lightdm',
 ]
 
 Facter.add('desktop_sessions') do
-  confine Facter.value(:os)['family'] => 'RedHat'
   setcode do
-    supported_desktop_sessions.select do |ds|
-      !Facter::Core::Execution.execute('rpm -q ' + Shellwords.escape(ds)).match(/ not installed$/)
-    end
-  end
-end
-
-Facter.add('desktop_sessions') do
-  confine Facter.value(:os)['family'] => 'Debian'
-  setcode do
-    supported_desktop_sessions.select do |ds|
-      Facter::Core::Execution.execute('dpkg -l ' + Shellwords.escape(ds)).match(/^ii/)
+    case Facter.value(:os)['family']
+    when 'RedHat'
+      supported_desktop_sessions.select do |ds|
+        !Facter::Core::Execution.execute('rpm -q ' + Shellwords.escape(ds)).match(/ not installed$/)
+      end
+    when 'Debian'
+      supported_desktop_sessions.select do |ds|
+        Facter::Core::Execution.execute('dpkg -l ' + Shellwords.escape(ds)).match(/^ii/)
+      end
+    else
+      []
     end
   end
 end
