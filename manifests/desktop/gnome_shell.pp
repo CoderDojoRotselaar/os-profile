7fe7d6ae51aafbb784eb70822018681ef579c0ff
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-05 16:31:24 +0100

    Configure auto-login for gnome
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/desktop/gnome_shell.pp
+++ manifests/desktop/gnome_shell.pp
@@ -1,2 +1,15 @@
 class profile::desktop::gnome_shell {
+  $gnome_config = {
+    'daemon' => {
+      AutomaticLoginEnable => 'True',
+      AutomaticLogin       => 'username',
+    }
+  }
+
+  $gnome_defaults = {
+    path              => '/etc/gdm/custom.conf',
+    key_val_separator => '=',
+  }
+
+  create_ini_settings($gnome_config, $gnome_defaults)
 }
