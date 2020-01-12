b90be3e3222ce5ac2637d864f71b5400d0af021a
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-13 00:27:14 +0100

    Install language packs too
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/locale.pp
+++ manifests/locale.pp
@@ -1,6 +1,23 @@
 class profile::locale (
   String $lang = 'en_US.UTF-8',
+  String $language = 'en',
 ) {
+  $langpacks = $facts['os']['family'] ? {
+    'Debian' => [
+      "aspell-${language}",
+      "firefox-locale-${language}",
+      "language-pack-${language}",
+      "language-pack-gnome-${language}",
+    ],
+    'RedHat' => ["langpacks-${language}"],
+    default  => [],
+  }
+
+  package { $langpacks:
+    ensure => installed,
+    before => Exec["Change system locale to ${lang}"],
+  }
+
   exec { "Change system locale to ${lang}":
     command => "/usr/bin/localectl set-locale LANG=${lang}",
     unless  => "/usr/bin/locale | grep -q '^LANG=${lang}$'"
