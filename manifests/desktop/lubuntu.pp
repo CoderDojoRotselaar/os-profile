e7cafa58130cd6c7e23c04ec4ffda9fe0803dab6
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-13 15:26:42 +0100

    Add requirement
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/desktop/lubuntu.pp
+++ manifests/desktop/lubuntu.pp
@@ -1,6 +1,7 @@
 class profile::desktop::lubuntu {
   package { 'lubuntu-desktop':
     ensure => installed,
+    before => User[$::profile::user::coderdojo_user],
   }
   service { 'lightdm':
     ensure => enabled,
