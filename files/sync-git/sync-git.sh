0a6b0f4c579eddb594878344420173d775255456
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-13 21:46:04 +0100

    Add sudo
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- files/sync-git/sync-git.sh
+++ files/sync-git/sync-git.sh
@@ -11,5 +11,5 @@ while true; do
   inotifywait -r -e modify,attrib,close_write,move,create,delete /var/lib/coderdojo/projects
   sudo -u coderdojo git -C /var/lib/coderdojo/projects add -A .
   sudo -u coderdojo git -C /var/lib/coderdojo/projects commit -m "Autocommit from $HOSTNAME"
-  GIT_SSH_COMMAND='ssh -i /var/lib/coderdojo/projects/.repokey' git -C /var/lib/coderdojo/projects push
+  GIT_SSH_COMMAND='ssh -i /var/lib/coderdojo/projects/.repokey' sudo -i -u coderdojo git -C /var/lib/coderdojo/projects push
 done
