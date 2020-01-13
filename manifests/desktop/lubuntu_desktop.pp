2e23bac88fb8455e4ace0e17fb7e5d12a773a494
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-13 18:16:57 +0100

    Switch to different file
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/desktop/lubuntu_desktop.pp
+++ manifests/desktop/lubuntu_desktop.pp
@@ -4,19 +4,41 @@ class profile::desktop::lubuntu_desktop {
     before => User[$::profile::user::coderdojo_user],
   }
 
-  file { [
-    "${::profile::user::coderdojo_home}/.config/lxpanel",
-    "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu",
-    "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu/panes",
-    ]:
-      ensure => directory,
-      owner  => $::profile::user::coderdojo_user,
-      group  => $::profile::user::coderdojo_group,
+  $unquoted_ini = {
+    'quote_char' => '',
   }
 
-  file { "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu/panes/panel":
-    ensure  => file,
-    content => file('profile/lubuntu_desktop/panel.conf'),
+  $desktop_config = {
+    '*' => {
+      'wallpaper_mode'        => 'crop',
+      'wallpaper_common'      => 1,
+      'wallpapers_configured' => 1,
+      'wallpaper'             => '/usr/share/backgrounds/coderdojo/coderdojo_background.png',
+      'wallpaper0'            => '/usr/share/backgrounds/coderdojo/coderdojo_background.png',
+      'desktop_bg'            => '#2e4060',
+      'desktop_fg'            => '#ffffff',
+      'desktop_shadow'        => '#000000',
+      'desktop_font'          => 'Ubuntu 11',
+      'show_wm_menu'          => 0,
+      'sort'                  => 'mtime;ascending;',
+      'show_documents'        => 1,
+      'show_trash'            => 1,
+      'show_mounts'           => 0,
+    },
+  }
+
+  file { [
+    "${::profile::user::coderdojo_home}/.config/pcmanfm",
+    "${::profile::user::coderdojo_home}/.config/pcmanfm/lubuntu",
+  ]:
+    ensure => directory,
+    owner  => $::profile::user::coderdojo_user,
+    group  => $::profile::user::coderdojo_group,
+  }
+
+  file { "${::profile::user::coderdojo_home}/.config/pcmanfm/lubuntu/desktop-items-0.conf":
+    ensure  => present,
+    content => hash2ini($desktop_config, $unquoted_ini),
     owner   => $::profile::user::coderdojo_user,
     group   => $::profile::user::coderdojo_group,
   }
