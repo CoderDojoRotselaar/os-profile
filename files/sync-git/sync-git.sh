c4605dbe709b694a70de213d598f5f4232fa27a9
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-13 21:45:25 +0100

    Fix paths
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- files/sync-git/sync-git.sh
+++ files/sync-git/sync-git.sh
@@ -2,14 +2,14 @@
 
 (
   while true; do
-    sudo -u coderdojo git -C /var/lib/coderdojo-projects pull
+    sudo -u coderdojo git -C /var/lib/coderdojo/projects pull
     sleep 10
   done
 ) &
 
 while true; do
-  inotifywait -r -e modify,attrib,close_write,move,create,delete /var/lib/coderdojo-projects
-  sudo -u coderdojo git -C /var/lib/coderdojo-projects add -A .
-  sudo -u coderdojo git -C /var/lib/coderdojo-projects commit -m "Autocommit from $HOSTNAME"
-  GIT_SSH_COMMAND='ssh -i /root/.ssh/coderdojo-deploy-key' git -C /var/lib/coderdojo-projects push
+  inotifywait -r -e modify,attrib,close_write,move,create,delete /var/lib/coderdojo/projects
+  sudo -u coderdojo git -C /var/lib/coderdojo/projects add -A .
+  sudo -u coderdojo git -C /var/lib/coderdojo/projects commit -m "Autocommit from $HOSTNAME"
+  GIT_SSH_COMMAND='ssh -i /var/lib/coderdojo/projects/.repokey' git -C /var/lib/coderdojo/projects push
 done
