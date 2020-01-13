2fc78d8e75d44812c6dee1b4129aa7aaf70e366f
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-13 17:08:01 +0100

    Add lubuntu panel config
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/desktop/lubuntu.pp
+++ manifests/desktop/lubuntu.pp
@@ -1,8 +1,28 @@
 class profile::desktop::lubuntu {
+  $background = $::profile::desktop::background
+
   package { 'lubuntu-desktop':
     ensure => installed,
     before => User[$::profile::user::coderdojo_user],
   }
+
+  file { [
+    "${::profile::user::coderdojo_home}/.config/lxpanel"
+    "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu"
+    "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu/panes"
+    ]:
+      ensure => directory,
+      owner  => $::profile::user::coderdojo_user,
+      group  => $::profile::user::coderdojo_group,
+  }
+
+  file { "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu/panes/panel":
+    ensure  => file,
+    content => template('profile/lubuntu/panel.conf.erb'),
+    owner   => $::profile::user::coderdojo_user,
+    group   => $::profile::user::coderdojo_group,
+  }
+
   service { 'lightdm':
     ensure => enabled,
   }
