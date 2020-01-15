268643064af567971043ecce68776d4b9d3f75eb
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-15 20:49:11 +0100

    add dir config
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/secrets.pp
+++ manifests/secrets.pp
@@ -3,19 +3,27 @@ class profile::secrets (
   Optional[String] $key_path = undef,
 ) {
   if $secret_repo {
+    file { '/root/secrets':
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0700',
+      require => Vcsrepo['/root/secrets'],
+    }
+
     vcsrepo { '/root/secrets':
-      ensure   => present,
+      ensure   => latest,
       user     => 'root',
-      mode     => '0700',
       provider => git,
       source   => $secret_repo,
     }
 
     if $key_path {
       exec { 'unlock /root/secrets':
-        command => "/usr/bin/git-crypt unlock ${key_path}",
-        cwd     => '/root/secrets',
-        require => Vcsrepo['/root/secrets'],
+        command     => "/usr/bin/git-crypt unlock ${key_path}",
+        cwd         => '/root/secrets',
+        require     => Vcsrepo['/root/secrets'],
+        refreshonly => true,
       }
     }
   }
