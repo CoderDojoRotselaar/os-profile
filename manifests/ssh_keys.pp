f226bdd1efc016ac0cc8ffd1f40690b276593d85
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-16 19:15:21 +0100

    Fully unique names
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/ssh_keys.pp
+++ manifests/ssh_keys.pp
@@ -4,12 +4,10 @@ class profile::ssh_keys (
   $keys.each |$k_name, $k| {
     ssh_authorized_key {
       "${k_name}:${::profile::user::coderdojo_user}":
-        name => $k_name,
         user => $::profile::user::coderdojo_user,
         *    => $k,
         ;
       "${k_name}:root":
-        name => $k_name,
         user => 'root',
         *    => $k,
         ;
