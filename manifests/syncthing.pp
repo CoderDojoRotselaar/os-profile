32ad59b007edc9d7820fdbf746866b3e9aab77d8
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-18 12:28:07 +0100

    Only expect password
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/syncthing.pp
+++ manifests/syncthing.pp
@@ -1,6 +1,5 @@
 class profile::syncthing (
   Optional[String] $password = undef,
-  Optional[String] $password_salt = undef,
 ) {
   file { '/etc/syncthing':
     ensure => directory,
@@ -12,7 +11,6 @@ class profile::syncthing (
   ::syncthing::instance { 'coderdojo-projects':
     home_path         => '/etc/syncthing/coderdojo-projects',
     gui_password      => $password,
-    gui_password_salt => $password_salt,
     require           => File['/etc/syncthing'],
   }
 }
