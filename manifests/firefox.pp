c029b368d204fc36fb024ca187e8be46ca39bf49
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-18 16:53:28 +0100

    Extract a firefox profile
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/firefox.pp
+++ manifests/firefox.pp
@@ -13,4 +13,28 @@ class profile::firefox {
     mode    => '0755',
     require => Package['firefox'],
   }
+
+  file {
+    "${coderdojo_home}/.mozilla":
+      ensure => directory,
+      owner  => $::profile::user::coderdojo_user,
+      group  => $::profile::user::coderdojo_group,
+      ;
+    "${coderdojo_home}/.mozilla/firefox":
+      ensure => directory,
+      owner  => $::profile::user::coderdojo_user,
+      group  => $::profile::user::coderdojo_group,
+      ;
+  }
+
+  archive { 'firefox-profile.tar.bz2':
+    ensure       => present,
+    source       => 'file:///var/lib/puppet-deployment/assets/firefox-profile.tar.bz2',
+    extract      => true,
+    extract_path => "${coderdojo_home}/.mozilla/firefox",
+    creates      => "${coderdojo_home}/.mozilla/firefox/profile.ini",
+    user         => $::profile::user::coderdojo_user,
+    group        => $::profile::user::coderdojo_group,
+    require      => File["${coderdojo_home}/.mozilla/firefox"],
+  }
 }
