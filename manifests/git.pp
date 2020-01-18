ad47eeacfaa141840f10755c7353ee3c4a9f2e99
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-18 15:10:44 +0100

    Cleanup
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/git.pp
+++ manifests/git.pp
@@ -5,35 +5,28 @@ class profile::git (
   $coderdojo_user = $::profile::user::coderdojo_user
 
   if $repository {
-    file { '/var/lib/coderdojo':
-      ensure => directory,
-      owner  => $coderdojo_user,
-    }
-
-    file { '/var/lib/coderdojo/.repokey':
-      ensure => present,
-      owner  => $coderdojo_user,
-      source => '/root/.ssh/coderdojo-deploy-key',
-      mode   => '0600',
-    }
-
-    vcsrepo { '/var/lib/coderdojo/projects':
-      ensure             => present,
-      user               => $coderdojo_user,
-      provider           => git,
-      source             => $repository,
-      keep_local_changes => true,
-      require            => File['/var/lib/coderdojo'],
+    vcsrepo { "${coderdojo_home}/demo-projecten":
+      ensure   => latest,
+      user     => $coderdojo_user,
+      provider => git,
+      source   => $repository,
     }
   }
 
-  file { '/usr/local/sync-git':
-    ensure => directory,
-  }
+  file {
+    '/usr/local/sync-git':
+      ensure => absent,
+      ;
 
-  file { '/usr/local/sync-git/sync-git':
-    ensure  => present,
-    content => file('profile/sync-git/sync-git.sh'),
-    mode    => '0755',
+    '/usr/local/sync-git/sync-git':
+      ensure  => absent,
+      ;
+    '/var/lib/coderdojo':
+      ensure => absent,
+      ;
+
+    '/var/lib/coderdojo/.repokey':
+      ensure => absent,
+      ;
   }
 }
