3cddf0bbee5077d0537946973c2c9b438a371cbf
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-01-31 09:43:35 +0100

    Don't start update-notifier
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/upgrade/debian.pp
+++ manifests/upgrade/debian.pp
@@ -2,10 +2,18 @@ class profile::upgrade::debian {
   class { 'unattended_upgrades':
     enable       => 1,
     random_sleep => 600,
+    auto         => {
+      clean => 1,
+    }
   }
 
-  file { '/etc/apt/apt.conf':
-    ensure  => absent,
+  file {
+    '/etc/apt/apt.conf':
+      ensure  => absent,
+      ;
+    '/etc/xdg/autostart/update-notifier.desktop':
+      ensure  => absent,
+      ;
   }
 
   Package <| |> -> File['/etc/apt/apt.conf']
