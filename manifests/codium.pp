bcbc9da83fe3dd34d4fa48b45fefca5a88eb9e9b
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2021-09-06 15:32:58 +0200

    Fix path to desktop file
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/codium.pp
+++ manifests/codium.pp
@@ -12,7 +12,7 @@ class profile::codium (
 
   file { "${coderdojo_home}/Bureaublad/codium.desktop":
     ensure  => file,
-    source  => '/usr/share/applications/codium.desktop',
+    source  => '/snap/codium/166/usr/share/applications/codium.desktop',
     owner   => $::profile::user::coderdojo_user,
     group   => $::profile::user::coderdojo_group,
     mode    => '0755',
