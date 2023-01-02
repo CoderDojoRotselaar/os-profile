0431b940d9200b4370b88bb5c335ec8e1613530d
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2023-01-03 00:11:00 +0100

    Update more files
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- manifests/desktop/lubuntu_desktop.pp
+++ manifests/desktop/lubuntu_desktop.pp
@@ -4,61 +4,33 @@ class profile::desktop::lubuntu_desktop (
   include ::profile::desktop::lightdm
 
   package { [
-    'wmctrl', 'lxpanel', 'lubuntu-desktop',
+    'wmctrl', 'lubuntu-desktop',
   ]:
     ensure => installed,
     before => User[$::profile::user::coderdojo_user],
   }
 
-  $unquoted_ini = {
-    'quote_char' => '',
-  }
-
-  $desktop_config = {
-    '*' => {
-      'wallpaper_mode'        => 'crop',
-      'wallpaper_common'      => 1,
-      'wallpapers_configured' => 1,
-      'wallpaper'             => '/usr/share/backgrounds/coderdojo/coderdojo_background.png',
-      'wallpaper0'            => '/usr/share/backgrounds/coderdojo/coderdojo_background.png',
-      'desktop_bg'            => '#2e4060',
-      'desktop_fg'            => '#ffffff',
-      'desktop_shadow'        => '#000000',
-      'desktop_font'          => 'Ubuntu 11',
-      'show_wm_menu'          => 0,
-      'sort'                  => 'mtime;ascending;',
-      'show_documents'        => 1,
-      'show_trash'            => 1,
-      'show_mounts'           => 1,
-    },
-  }
-
-  file { [
-    "${::profile::user::coderdojo_home}/.config/pcmanfm",
-    "${::profile::user::coderdojo_home}/.config/pcmanfm/lubuntu",
-    "${::profile::user::coderdojo_home}/.config/lxpanel",
-    "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu",
-    "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu/panels",
-    "${::profile::user::coderdojo_home}/.config/clipit",
-  ]:
+  file { "${::profile::user::coderdojo_home}/.config/clipit":
     ensure => directory,
     owner  => $::profile::user::coderdojo_user,
     group  => $::profile::user::coderdojo_group,
   }
 
-  file { "${::profile::user::coderdojo_home}/.config/pcmanfm/lubuntu/desktop-items-0.conf":
-    ensure  => present,
-    content => hash2ini($desktop_config, $unquoted_ini),
-    owner   => $::profile::user::coderdojo_user,
-    group   => $::profile::user::coderdojo_group,
+  file { [
+    "${::profile::user::coderdojo_home}/.config/pcmanfm/",
+    "${::profile::user::coderdojo_home}/.config/lxpanel",
+  ]:
+    ensure  => absent,
+    recurse => true,
+    purge   => true,
+    force   => true,
   }
 
-  file { "${::profile::user::coderdojo_home}/.config/lxpanel/Lubuntu/panels/panel":
+  file { "${::profile::user::coderdojo_home}/.config/lxqt/panel.conf":
     ensure  => present,
-    content => file('profile/lubuntu_desktop/panels.conf'),
+    content => file('profile/lubuntu_desktop/lxqt_panel.conf'),
     owner   => $::profile::user::coderdojo_user,
     group   => $::profile::user::coderdojo_group,
-    notify  => Exec['Restart lxpanel'],
   }
 
   file { "${::profile::user::coderdojo_home}/.config/clipit/clipitrc":
@@ -70,16 +42,9 @@ class profile::desktop::lubuntu_desktop (
 
   file { "${::profile::user::coderdojo_home}/.config/pcmanfm-qt/lxqt/settings.conf":
     ensure  => present,
-    content => file('profile/lxqt_settings.conf'),
+    content => file('profile/lubuntu_desktop/lxqt_settings.conf'),
     owner   => $::profile::user::coderdojo_user,
     group   => $::profile::user::coderdojo_group,
-  }
-
-  exec { 'Restart lxpanel':
-    command     => '/usr/bin/lxpanelctl restart',
-    user        => $::profile::user::coderdojo_user,
-    refreshonly => true,
-    require     => Package['lxpanel'],
   }
 
   exec { "Set number of desktops to ${desktops}":
