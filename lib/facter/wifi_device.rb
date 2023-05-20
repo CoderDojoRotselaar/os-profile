8c68b03f65028ce91bcb159fb163fb4f17591aeb
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2023-05-20 19:44:13 +0200

    Use a function
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- lib/facter/wifi_device.rb
+++ lib/facter/wifi_device.rb
@@ -1,11 +1,13 @@
+def find_wifi
+  devices = %x(nmcli -t device).split("\n")
+  devices.each do |dev|
+    name, type, managed = dev.split(':')
+    return name if type == 'wifi'
+  end
+end
+
 Facter.add('wifi_device') do
   setcode do
-    devices = %x(nmcli -t device).split("\n")
-    wifi = devices.find do |dev|
-      name, type, managed = dev.split(':')
-      name if type == 'wifi'
-    end
-
-    wifi
+    find_wifi
   end
 end
